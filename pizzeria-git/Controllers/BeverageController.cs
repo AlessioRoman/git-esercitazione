7acd5af7c860dfd0b7dc492eee20723ff300aae0
AlessioRoman <alessio.romano02dev@gmail.com>
2023-05-24 17:14:49 +0200

    controller fixes

--- pizzeria-git/Controllers/BeverageController.cs
+++ pizzeria-git/Controllers/BeverageController.cs
@@ -4,9 +4,56 @@ namespace pizzeria_git.Controllers
 {
     public class BeverageController : Controller
     {
-        public IActionResult Index()
+        public class PizzaController : Controller
         {
-            return View();
+            //public IActionResult Index()
+            //{
+            //    using (BeverageController db = new())
+            //    {
+            //        List<BeverageController> pizze = db.Pizzas.ToList();
+            //        return View(pizze);
+            //    }
+            //}
+
+            //[HttpGet]
+            //public IActionResult Create()
+            //{
+            //    return View();
+            //}
+
+            //[HttpPost]
+            //[ValidateAntiForgeryToken]
+            //public IActionResult Create(PizzaModel newPizza)
+            //{
+            //    if (!ModelState.IsValid)
+            //    {
+            //        return View("Create", newPizza);
+            //    }
+
+            //    using (PizzaContext db = new())
+            //    {
+            //        db.Pizzas.Add(newPizza);
+            //        db.SaveChanges();
+
+            //        return RedirectToAction("Index");
+            //    }
+            //}
+
+            //public IActionResult Details(int id)
+            //{
+            //    using (PizzaContext db = new())
+            //    {
+            //        PizzaModel? pizzaDetails = db.Pizzas.Where(PizzaModel => PizzaModel.Id == id).FirstOrDefault();
+
+            //        if (pizzaDetails != null)
+            //        {
+            //            return View("Details", pizzaDetails);
+            //        }
+            //        else
+            //        {
+            //            return NotFound($"L'articolo con id {id} non è stato trovato!");
+            //        }
+            //    }
         }
     }
 }
